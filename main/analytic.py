0706b4bb90a1c40b47734a1c072837b807ea18f7
jinho-laptop <woal777@naver.com>
2018-07-26 23:56:30 +0900

    sze

--- main/analytic.py
+++ main/analytic.py
@@ -1,24 +1,25 @@
 from cmath import exp, sqrt, sinh, pi
-import matplotlib.pyplot as plt
-import numpy as np
 
-c = 3e+8  # m/s
-m0 = 0.5109989461e+6 / (c ** 2)  # ev / c ** 2
+m0 = 0.511 * 1e+6 / 299792458 ** 2  # eV * (s / m) ** 2
+h = 4.135667662 * 1e-15  # eV * s
 h_ = 6.582119514e-16  # ev s
 e = 1.602e-19  # C
+q = e
+k = 8.617343e-5  # eV / K
+rich = 4 * pi * m0 * e * k ** 2 / h ** 3
 
 
-def di(v, p1, p2, mr):
+def di(d, v, p1, p2, mr):
     m = mr * m0
     C = - (4 * e * m) / (9 * pi ** 2 * h_ ** 3)
     alpha = (4 * d * (2 * m) ** .5) / (3 * h_ * (p1 + v - p2))
     term_sinh: complex = 1.5 * alpha * (sqrt(p2 - v / 2) - sqrt(p1 + v / 2)) * v / 2
     term_exp: complex = alpha * (sqrt(p2 - v / 2) ** 3 - sqrt(p1 + v / 2) ** 3)
-    term_reciprocal: complex = (alpha ** 2 * (sqrt(p2 - v / 2) - sqrt(p1 + v / 2)) ** 2)
-    return (C * exp(term_exp.real).real * sinh(term_sinh.real).real / term_reciprocal.real).real
+    term_rec: complex = (alpha ** 2 * (sqrt(p2 - v / 2) - sqrt(p1 + v / 2)) ** 2)
+    return (C * exp(term_exp) * sinh(term_sinh) / term_rec).real
 
 
-def fn(v, p1, p2, mr):
+def fn(d, v, p1, p2, mr):
     m = mr * m0
     E = - v / (e * d) + (p2 - p1) / (e * d)
     C = e ** 3 * m0 / (16 * pi ** 2 * h_ * m)
@@ -30,75 +31,20 @@ def fn(v, p1, p2, mr):
     return (C * E ** 2 / p * exp(-4 * (2 * m) ** 0.5 * p ** 1.5 / (3 * h_ * e * E))).real
 
 
-if __name__ == "__main__":
-    a = input('2009? or 2015?\n')
-    if a == '09':
-        d = 4.8e-9  # m
-        # up
-        x = np.linspace(-.3, .3, 30)
-        area = 0.5e-6 ** 2
-        dimension = 1e+6
-        y = [-di(v, 0.24, 1.52, 1) * area * dimension for v in x]
-        plt.plot(-x, y)
+### sze ch8. TUNNEL DEVICES
 
-        # down
-        y = [-di(v, 0.48, .96, 1) * area * dimension for v in x]
-        plt.plot(-x, y)
-        plt.xlim((-.3, .3))
-        plt.show()
+def mis(temp, d, phi_t, shb, eta, v, mr):
+    alpha = 2 * sqrt(2 * e * mr) / h
+    return rich * temp ** 2 * exp(-alpha * d * sqrt(e * phi_t) - e * shb / k / temp) * (exp(e * v / eta / k / temp) - 1)
 
-    elif a == '15':
-        d = 4.6e-9  # m
-        start = -0.5
-        end = -start
-        dimension = 1e-4
-        # W
-        x = np.linspace(start, end, 31)
-        yW = [(di(v, 0.2, 1.45, .22) + fn(v, 0.2, 1.45, .22) * .24) * dimension for v in x]
 
-        # Co
-        yC = [(di(v, 0.2, 1.67, .20) + fn(v, 0.2, 1.67, .20) * .19) * dimension for v in x]
+def dt_sze(d, phi0, v, mr):
+    j0 = q ** 2 / (2 * pi * h * d ** 2)
+    c = 4 * pi * d * sqrt(2 * mr * q) / h
+    return j0 * (phi0 - v / 2) * exp(-c * sqrt(phi0 - v / 2)) - (phi0 + v / 2) * exp(-c * sqrt(phi0 + v / 2))
 
-        # Ni
-        yN = [(di(v, 0.2, 2.05, .20) + fn(v, 0.2, 2.05, .20) * .14) * dimension for v in x]
 
-        yI = [(di(v, 0.2, 2.84, .16) + fn(v, 0.2, 2.84, .16) * .07) * dimension for v in x]
-        plt.plot(x, yW, x, yC, x, yN, x, yI)
-        tot = np.array((x, yW, yC, yN, yI)).transpose()
-        np.savetxt('15.dat', tot)
-        plt.xlim((start, end))
-        plt.show()
-
-    elif a == '14':
-        d = 3e-9  # m
-        start = -0.1
-        end = -start
-        eff_m = 1
-        area = 2e-12
-        dimension = 1e+9
-        x = np.linspace(start, end, 30)
-        y = [-di(v, 0.3, 0.61, eff_m) * area * dimension for v in x]
-
-        # down
-        y2 = [-di(v, 0.37, .76, eff_m) * area * dimension for v in x]
-        plt.plot(-x, y, -x, y2)
-        tot = np.array((-x, y, y2)).transpose()
-        np.savetxt('14.dat', tot)
-        plt.show()
-
-    elif a == '14au':
-        d = 3e-9  # m
-        start = -0.2
-        end = -start
-        eff_m = 1
-        area = 2e-12
-        dimension = 1e+9
-        x = np.linspace(start, end, 30)
-        y = [-di(v, 0.26, 0.78, eff_m) * area * dimension for v in x]
-
-        # down
-        y2 = [-di(v, 0.24, .92, eff_m) * area * dimension for v in x]
-        plt.plot(-x, y, -x, y2)
-        tot = np.array((-x, y, y2)).transpose()
-        np.savetxt('14au.dat', tot)
-        plt.show()
+def fn_sze(phi0, ep, v, mr):
+    ep0 = 8 * pi / 3 / h * sqrt(2 * mr * q) * phi0 ** 1.5
+    j0 = q ** 2 * pow(e, 2) / (4 * pi * h * phi0) * (
+                exp(-ep0 / ep) - (1 + 2 * v / phi0) * exp(-ep0 / ep * sqrt(1 + (2 * v / phi0))))
--- main/currunt_analytic.py
+++ main/currunt_analytic.py
@@ -1,49 +1,77 @@
-from math import *
+from main.analytic import *
 import matplotlib.pyplot as plt
-
-m = 0.511 * 1e+6 / 299792458 ** 2  # eV * (s / m) ** 2
-h = 4.135667662 * 1e-15  # eV * s
-hbar = 6.582119514 * 1e-16  # eV * s
-electron = 1.602e-19  # C
-d = 3e-09
-
-c = 4 * electron * m / (9 * pi ** 2 * hbar ** 3)
+import numpy as np
 
 
-def dt(ph1, ph2, V):
-    a = 4 * d * (2 * m) ** 0.5 / (3 * hbar * (ph1 + V - ph2))
-    return -c * exp(a * ((ph2 - V / 2) ** 1.5 - (ph1 + V / 2) ** 1.5)) / (a * (
-        (ph2 - V / 2) ** 0.5 - (ph1 + V / 2) ** 0.5)) ** 2 * sinh(
-        0.75 * V * a * ((ph2 - V / 2) ** 0.5 - (ph1 + V / 2)))
+if __name__ == "__main__":
+    a = input('2009? or 2015?\n')
+    if a == '09':
+        d = 4.8e-9  # m
+        # up
+        x = np.linspace(-.3, .3, 30)
+        area = 0.5e-6 ** 2
+        dimension = 1e+6
+        y = [-di(v, 0.24, 1.52, 1) * area * dimension for v in x]
+        plt.plot(-x, y)
 
+        # down
+        y = [-di(v, 0.48, .96, 1) * area * dimension for v in x]
+        plt.plot(-x, y)
+        plt.xlim((-.3, .3))
+        plt.show()
 
-x = [7.75E-04,
-     0.008139535,
-     0.016666668,
-     0.02868217,
-     0.043023255,
-     0.05658915,
-     0.069379844,
-     0.08527132,
-     0.09612403,
-     0.10930233
-     ]
-arr = []
-arr2 = []
-for i in x:
-    arr.append(dt(0.4889338, 1.0729764, i) * 2e-06 * 1e+09)
-    arr2.append(dt(0.46554133, 0.73044676, i) * 2e-06 * 1e+09)
-plt.plot(x, arr, x, arr2)
-plt.show()
-'''
--0.001773836
-0.014190688
-0.031929046
-0.054988913
-0.09578714
-0.12239468
-0.1631929
-0.20044346
-0.23769401
-0.27849224
-'''
+    elif a == '15':
+        d = 4.6e-9  # m
+        start = -0.5
+        end = -start
+        dimension = 1e-4
+        # W
+        x = np.linspace(start, end, 31)
+        yW = [(di(v, 0.2, 1.45, .22) + fn(v, 0.2, 1.45, .22) * .24) * dimension for v in x]
+
+        # Co
+        yC = [(di(v, 0.2, 1.67, .20) + fn(v, 0.2, 1.67, .20) * .19) * dimension for v in x]
+
+        # Ni
+        yN = [(di(v, 0.2, 2.05, .20) + fn(v, 0.2, 2.05, .20) * .14) * dimension for v in x]
+
+        yI = [(di(v, 0.2, 2.84, .16) + fn(v, 0.2, 2.84, .16) * .07) * dimension for v in x]
+        plt.plot(x, yW, x, yC, x, yN, x, yI)
+        tot = np.array((x, yW, yC, yN, yI)).transpose()
+        np.savetxt('15.dat', tot)
+        plt.xlim((start, end))
+        plt.show()
+
+    elif a == '14':
+        d = 3e-9  # m
+        start = -0.1
+        end = -start
+        eff_m = 1
+        area = 2e-12
+        dimension = 1e+9
+        x = np.linspace(start, end, 30)
+        y = [-di(v, 0.3, 0.61, eff_m) * area * dimension for v in x]
+
+        # down
+        y2 = [-di(v, 0.37, .76, eff_m) * area * dimension for v in x]
+        plt.plot(-x, y, -x, y2)
+        tot = np.array((-x, y, y2)).transpose()
+        np.savetxt('14.dat', tot)
+        plt.show()
+
+    elif a == '14au':
+        d = 3e-9  # m
+        start = -0.2
+        end = -start
+        eff_m = 1
+        area = 2e-12
+        dimension = 1e+9
+        x = np.linspace(start, end, 30)
+        y = [-di(v, 0.26, 0.78, eff_m) * area * dimension for v in x]
+
+        # down
+        y2 = [-di(v, 0.24, .92, eff_m) * area * dimension for v in x]
+        plt.plot(-x, y, -x, y2)
+        tot = np.array((-x, y, y2)).transpose()
+        np.savetxt('14au.dat', tot)
+        plt.show()
